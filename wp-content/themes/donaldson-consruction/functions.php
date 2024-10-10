6a8677fa0024d94f45f061db9b8ed4331b4b8fd1
jxhnkndl <jkroyston@gmail.com>
2024-10-10 16:01:31 -0400

    remove default fields from additional custom post types

--- wp-content/themes/donaldson-consruction/functions.php
+++ wp-content/themes/donaldson-consruction/functions.php
@@ -1,12 +1,13 @@
 <?php
 
   // Remove default Wordpress fields to make room for advanced custom fields
-  function jkr_remove_default_page_fields() {
+  function jkr_remove_default_fields() {
     remove_post_type_support( 'page', 'editor' );
     remove_post_type_support( 'post', 'editor' );
     remove_post_type_support( 'project', 'editor' );
+    remove_post_type_support( 'testimonial', 'editor' );
   }
 
-  add_action( 'init', 'jkr_remove_default_page_fields' );
+  add_action( 'init', 'jkr_remove_default_fields' );
 
 ?>
