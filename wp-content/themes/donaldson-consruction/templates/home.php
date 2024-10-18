177ebc20f1de2fea1b52ed528ce7280d8884d4f6
jxhnkndl <jkroyston@gmail.com>
2024-10-18 14:49:13 -0400

    create brand block components

--- wp-content/themes/donaldson-consruction/templates/home.php
+++ wp-content/themes/donaldson-consruction/templates/home.php
@@ -2,15 +2,27 @@
 <?php get_header(); ?>
 
 <?php 
+  // Main content
   $main_heading = get_field( 'main_heading' );
   $mission_statement = get_field( 'mission_statement' );
   $cta_button_label = get_field( 'cta_button_label' );
   $featured_projects_heading = get_field( 'featured_projects_heading' );
   $featured_projects_button_label = get_field( 'featured_projects_button_label' );
   $testimonials_heading = get_field( 'testimonials_heading' );
+
+  // Brand block top
+  $brand_block_top = get_field( 'brand_block_top' );
+  $brand_block_top_heading = $brand_block_top[ 'brand_statement' ];
+  $brand_block_top_image = $brand_block_top[ 'brand_image' ][ 'url' ];
+
+  // Brand block bottom
+  $brand_block_bottom = get_field( 'brand_block_bottom' );
+  $brand_block_bottom_heading = $brand_block_bottom[ 'brand_statement' ];
+  $brand_block_bottom_image = $brand_block_bottom[ 'brand_image' ][ 'url' ];
 ?>
 
 <main>
+  
   <!-- Hero Image Carousel -->
   <?php 
   $images = get_field( 'hero_images' ); 
@@ -34,7 +46,30 @@
       <div class="btn btn-dark"><?php echo esc_html( $cta_button_label ) ?></div>
     </div>
   </section>
-
+  <!-- Brand Block -->
+  <section class="brand-block-section">
+    <div class="blueprint-bg">
+      <div class="overlay overlay-primary">
+        <h2 class="brand-block-heading"><?php echo esc_html( $brand_block_top_heading ) ?></h2>
+      </div>
+    </div>
+    <div class="brand-image" style="background-image: url(<?php echo esc_url( $brand_block_top_image ) ?>); background-size: cover; background-position: center; background-repeat: no-repeat;"></div>
+  </section>
+  <!-- Featured Projects -->
+  <div class="content-section">
+    <div class="content-container">
+      <h2><?php echo esc_html( $featured_projects_heading ); ?></h2>
+    </div>
+  </div>
+    <!-- Brand Block -->
+  <section class="brand-block-section">
+    <div class="brand-image" style="background-image: url(<?php echo esc_url( $brand_block_bottom_image ) ?>); background-size: cover; background-position: center; background-repeat: no-repeat;"></div>
+    <div class="blueprint-bg">
+      <div class="overlay overlay-secondary">
+        <h2 class="brand-block-heading"><?php echo esc_html( $brand_block_bottom_heading ) ?></h2>
+      </div>
+    </div>
+  </section>
 </main>
 
 <?php get_footer(); ?>
