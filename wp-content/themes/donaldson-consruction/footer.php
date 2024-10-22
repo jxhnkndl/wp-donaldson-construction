74d2a35584b77c349eca1d8a4e2aaed6d8c3c062
jxhnkndl <jkroyston@gmail.com>
2024-10-22 12:21:03 -0400

    start piecing together footer

--- wp-content/themes/donaldson-consruction/footer.php
+++ wp-content/themes/donaldson-consruction/footer.php
@@ -1,5 +1,30 @@
   <footer>
-
+    <div class="content-container">
+      <div class="footer-grid">
+        <div class="footer-cell footer-links">
+          <h4 class="footer-column-heading">Navigate</h4>
+          <?php
+            wp_nav_menu(
+              array(
+                'menu' => 'Footer Menu',
+                'container' => '',
+                'theme_location' => 'Footer Menu Items',
+                'items_wrap' => '<ul class="footer-nav-links">%3$s</ul>'
+              )
+            );
+          ?>
+        </div>
+        <div class="footer-cell footer-contact">
+          <h4 class="footer-column-heading">Contact</h4>
+        </div>
+        <div class="footer-cell footer-notices">
+          <h4 class="footer-column-heading">Notices</h4>
+        </div>
+        <div class="footer-cell footer-connect">
+          <h4 class="footer-column-heading">Connect</h4>
+        </div>
+      </div>
+    </div>
   </footer>
 
   <?php wp_footer(); ?>
