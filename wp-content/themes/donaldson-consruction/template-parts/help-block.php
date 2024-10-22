bd27e6483bd83fe40937bf367547e92d0f3219b2
jxhnkndl <jkroyston@gmail.com>
2024-10-22 14:13:52 -0400

    add narrow version of help block and adjust styling on wide version of help block

--- wp-content/themes/donaldson-consruction/template-parts/help-block.php
+++ wp-content/themes/donaldson-consruction/template-parts/help-block.php
@@ -2,11 +2,12 @@
   $heading = get_field( 'help_block_heading', 'option' );
   $body = get_field( 'help_block_body', 'option' );
   $button_text = get_field( 'help_block_button_text', 'option' );
-  $image = get_field( 'help_block_image', 'option' );
+  $image_wide = get_field( 'help_block_image_wide', 'option' );
+  $image_tall = get_field( 'help_block_image_tall', 'option' );
 ?>
 
 <section class="help-block-wide">
-  <div class="bg-image" style="background-image: url(<?php echo $image[ 'url' ]; ?>); ">
+  <div class="bg-image" style="background-image: url(<?php echo $image_wide[ 'url' ]; ?>); ">
     <div class="bg-blur"></div>
     <div class="bg-placeholder"></div>
   </div>
@@ -19,5 +20,18 @@
       </div>
     </div>
     <div class="bg-orange"></div>
+  </div>
+</section>
+
+<section class="help-block-tall">
+  <div class="bg-image-tall" style="background-image: url(<?php echo $image_tall[ 'url' ]; ?>); ">
+    <div class="bg-image-tall-overlay">
+      <div class="bg-image-tall-blur"></div>
+      <div class="bg-image-tall-content">
+        <h3><?php echo esc_html( $heading ); ?></h3>
+        <p><?php echo esc_html( $body ); ?></p>
+        <a href="#" class="btn btn-secondary"><?php echo $button_text; ?></a>
+      </div>
+    </div>
   </div>
 </section>
