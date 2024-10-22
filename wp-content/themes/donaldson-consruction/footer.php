c3011c260744e7754661ecb2aaca87dc18c31cdd
jxhnkndl <jkroyston@gmail.com>
2024-10-22 13:47:38 -0400

    create footer component

--- wp-content/themes/donaldson-consruction/footer.php
+++ wp-content/themes/donaldson-consruction/footer.php
@@ -1,3 +1,11 @@
+<?php  
+  $richmond_phone = get_field( 'richmond_phone_number', 'option' );
+  $charlottesville_phone = get_field( 'charlottesville_phone_number', 'option' );
+  $email_address = get_field( 'email_address', 'option' );
+  $facebook_url = get_field ( 'facebook_url', 'option' );
+  $instagram_url = get_field( 'instagram_url', 'option' );
+?> 
+  
   <footer>
     <div class="content-container">
       <div class="footer-grid">
@@ -9,19 +17,56 @@
                 'menu' => 'Footer Menu',
                 'container' => '',
                 'theme_location' => 'Footer Menu Items',
-                'items_wrap' => '<ul class="footer-nav-links">%3$s</ul>'
+                'items_wrap' => '<ul class="footer-nav-items">%3$s</ul>'
               )
             );
           ?>
         </div>
         <div class="footer-cell footer-contact">
           <h4 class="footer-column-heading">Contact</h4>
+          <ul class="footer-nav-items">
+            <li class="contact-item">
+              <a href="tel:<?php echo esc_attr( $charlottesville_phone ); ?>">
+                <i class="contact-icon fa-solid fa-phone"></i>
+                Charlottesville
+              </a>
+            </li>
+            <li class="contact-item">
+              <a href="tel:<?php echo esc_attr( $richmond_phone ); ?>">
+                <i class="contact-icon fa-solid fa-phone"></i>
+                Richmond
+              </a>
+            </li>
+            <li class="contact-item">
+              <a href="mailto:<?php echo esc_url( $email_address ); ?>">
+              <i class="contact-icon fa-solid fa-envelope"></i>
+                Email Us
+              </a>
+            </li>
+          </ul>
         </div>
         <div class="footer-cell footer-notices">
           <h4 class="footer-column-heading">Notices</h4>
+          <ul class="footer-nav-items">
+            <li>Copyright &copy; 2024</li>
+            <li>Donaldson Construction LLC</li>
+            <li>All Rights Reserved</li>
+            <li>Built by J.K. Royston</li>
+          </ul>
         </div>
         <div class="footer-cell footer-connect">
           <h4 class="footer-column-heading">Connect</h4>
+          <div class="social-icons">
+            <a href="<?php echo esc_url( $facebook_url ); ?>">
+              <i class="social-icon fa-brands fa-facebook"></i>
+            </a>
+            <a href="<?php echo esc_url( $instagram_url ); ?>">
+              <i class="social-icon fa-brands fa-instagram"></i>
+            </a>
+            <a href="mailto:<?php echo esc_url( $email_address ); ?>">
+              <i class="social-icon fa-solid fa-envelope"></i>
+            </a>
+          </div>
         </div>
       </div>
     </div>
