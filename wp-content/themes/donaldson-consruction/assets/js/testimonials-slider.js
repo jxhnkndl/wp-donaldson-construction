ef9dc9420bcb6dd2ee5124a3c3632a2494353537
jxhnkndl <jkroyston@gmail.com>
2024-10-29 13:20:15 -0400

    remove unused nav arrows from testimonials slider

--- wp-content/themes/donaldson-consruction/assets/js/testimonials-slider.js
+++ wp-content/themes/donaldson-consruction/assets/js/testimonials-slider.js
@@ -7,8 +7,6 @@ $(document).ready(() => {
     draggable: true,
     infinite: true,
     swipe: true
-    // nextArrow: '<div class="arrow-container"><svg class="testimonial-slider-arrow arrow-next" xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 512 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM297 385c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l71-71L120 280c-13.3 0-24-10.7-24-24s10.7-24 24-24l214.1 0-71-71c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0L409 239c9.4 9.4 9.4 24.6 0 33.9L297 385z"/></svg></div>',
-    // prevArrow: '<div class="arrow-container"><svg class="testimonial-slider-arrow arrow-prev" xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 512 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM215 127c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-71 71L392 232c13.3 0 24 10.7 24 24s-10.7 24-24 24l-214.1 0 71 71c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L103 273c-9.4-9.4-9.4-24.6 0-33.9L215 127z"/></svg></div>',
   });
 
   $('.testimonial-slide').removeAttr('style');
