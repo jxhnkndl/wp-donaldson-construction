46adde93ccd697d4b7fa06974a7ed9a44080bf2c
jxhnkndl <jkroyston@gmail.com>
2024-10-30 11:35:42 -0400

    add animation to nav links

--- wp-content/themes/donaldson-consruction/assets/js/main-nav.js
+++ wp-content/themes/donaldson-consruction/assets/js/main-nav.js
@@ -5,6 +5,9 @@ const menuBarBottomEl = document.querySelector('.menu-bar-bottom');
 const navEl = document.querySelector('.nav-drawer');
 const overlayEl = document.querySelector('.opacity-overlay');
 
+const navLinksEl = document.querySelectorAll('.nav-links-list li');
+const navInfoEl = document.querySelector('.nav-info-container');
+
 menuEl.addEventListener('click', () => {
   // Determine whether nav drawer is open or closed
   const isOpen = JSON.parse(menuEl.getAttribute('aria-expanded'));
@@ -25,32 +28,52 @@ overlayEl.addEventListener('click', () => {
 
 const openNav = () => {
   navEl.style.display = 'flex';
-    overlayEl.style.display = 'block';
+  overlayEl.style.display = 'block';
   
-    gsap.to('.opacity-overlay', {
-      duration: 0.25,
-      opacity: 0.8
-    });
+  gsap.to('.opacity-overlay', {
+    duration: 0.25,
+    opacity: 0.8
+  });
   
-    gsap.to('.nav-drawer', {
-      duration: 0.25,
-      opacity: 1
-    });
+  gsap.to('.nav-drawer', {
+    duration: 0.25,
+    opacity: 1
+  });
+  
+  gsap.to('.menu-bar-top', {
+    duration: 0.25,
+    width: 30,
+    height: 3,
+    y: 6,
+    rotation: 45
+  });
+  
+  gsap.to('.menu-bar-bottom', {
+    duration: 0.25,
+    height: 3,
+    y: -4,
+    rotation: -45
+  });
 
-    gsap.to('.menu-bar-top', {
-      duration: 0.25,
-      width: 30,
-      height: 3,
-      y: 6,
-      rotation: 45
-    });
-    
-    gsap.to('.menu-bar-bottom', {
-      duration: 0.25,
-      height: 3,
-      y: -4,
-      rotation: -45
-    });
+  let linkAnimationDelay = 0.2;
+
+  navLinksEl.forEach((link, i) => {
+    if (i !== 0) {
+      linkAnimationDelay += 0.1;
+    }
+
+    gsap.fromTo(
+      link,
+      { opacity: 0, x: -40},
+      { delay: linkAnimationDelay, duration: 0.25, opacity: 1, x: 0 }
+    )
+  });
+
+  gsap.fromTo(
+    navInfoEl,
+    { opacity: 0, x: -40 },
+    { delay: linkAnimationDelay += 0.2, duration: 0.25, opacity: 1, x: 0}
+  )
 }
 
 const closeNav = () => {
